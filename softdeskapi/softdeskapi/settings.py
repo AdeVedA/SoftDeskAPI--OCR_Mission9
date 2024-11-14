aa586051e0a3ad12e5c0abcdea54a30c096683ba
Ju_M <julienmarc@free.fr>
2024-11-14 10:03:22 +0100

    Throttling in settings

--- softdeskapi/softdeskapi/settings.py
+++ softdeskapi/softdeskapi/settings.py
@@ -142,6 +142,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # configure django-rest-framework permissions to accept JSON Web Tokens
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,  # Limite à 10 objets par page (ajustez en fonction des besoins)
 }
